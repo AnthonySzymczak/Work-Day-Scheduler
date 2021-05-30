116c2b153ca3f16da7b8bc43fa715e62d3a2b6dc
AnthonySzymczak <doyalajs1@live.com>
2021-05-30 15:24:59 -0400

    Clock added, fixing local storage

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
 //Calls document first
-$(document).ready(function(){
-    let tasks = {};
+$(document).ready(function () {
+  //Current Date
 
-    //TIME ARRAY
-    let myTimeArray = [
-        $('#9'),
-        $('#10'),        
-        $('#11'),
-        $('#12'),
-        $('#13'),
-        $('#14'),
-        $('#15'),
-        $('#16'),
-        $('#17'),
-    ]
+  let today = document.querySelector("#currentDay");
+  let currentTime = moment();
+  today.textContent = currentTime.format("MMMM DD, YYYY ");
 
-    //Load Tasks
-    let loadTasks = function() {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
+  //TIME ARRAY
+  let myTimeArray = [
+    $("#9"),
+    $("#10"),
+    $("#11"),
+    $("#12"),
+    $("#13"),
+    $("#14"),
+    $("#15"),
+    $("#16"),
+    $("#17"),
+  ];
+  // Time Loop to recognize past time, present time, and future time.
+  function timeLoop() {
+    for (let i = 0; i < myTimeArray.length; i++) {
+      //Formating Current Date with Moment.js
+      let terasDate = moment().hours();
+      //Hour declaration
+      let godzina = parseInt($(".area")[i].id);
+      //Now ELement Declaration
+      let terasElem = $(".area")[i];
+
+      if (godzina < terasDate) {
+        terasElem.classList.add("past");
+      } else if (godzina === terasDate) {
+        terasElem.classList.add("present");
+        terasElem.classList.remove("past");
+      } else {
+        terasElem.classList.add("future");
+        terasElem.classList.remove("present");
+        terasElem.classList.remove("past");
+      }
     }
+  }
+  const clock = document.getElementById('clock');
 
-    //Save Button & Localstorage setup
-    let saveTasks = function() {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    };     
-    $(".saveBtn").on("click", function(){
-    })
-    // Time Loop
-    function timeLoop(){
-        for (let i = 0; i < myTimeArray.length; i++){
-            //Formating Current Date with Moment.js
-            let terasDate = moment().hours();
-            //Hour declaration
-            let godzina = parseInt($(".area") [i].id);
-            //Now ELement Declaration
-            let terasElem = $(".area")[i];
+  function updateTime() {
+        const now = moment();
+        const realTime = now.format('hh:mm:ss A');
 
-            if (godzina < terasDate){
-                terasElem.classList.add('past');
-            }else if(godzina === terasDate){
-                terasElem.classList.add('present');
-                terasElem.classList.remove('past');
-            } else {
-                terasElem.classList.add('future');
-                terasElem.classList.remove('present');
-                terasElem.classList.remove('past');
-            }   
-        }
-    }
-    timeLoop();
+        clock.textContent = realTime;
 
+  };
+  setInterval(updateTime,1000);
+  updateTime();
 
-})
+let storedItem = localStorage.getItem("storedItem");
+
+function save(){
+    let item = document.getElementById("savedText").val;
+    localStorage.setItem("storedItem", item);
+    document.getElementById("savedText").innerHTML = item;
+}
+function get(){
+    localStorage.getItem("storedItem");
+    document.getElementById("returnedText").innerHTML = storedItem ;
+}
+  
+
+  timeLoop();
+  save();
+  get();
+});
